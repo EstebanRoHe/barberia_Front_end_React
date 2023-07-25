4d490412868586e0461c8c0b3c0fd0cb72c74e0a
EstebanRoHe <esteban_rojashe@hotmail.com>
2023-07-25 02:50:23 -0600

    Update http-common.js

--- src/http-common.js
+++ src/http-common.js
@@ -1,11 +1,9 @@
 import axios from 'axios';
 
 const api = axios.create({
-  
- baseURL: 'https://barberia.onrender.com',
+  baseURL: 'https://barberia.onrender.com',
   headers: {
     'Content-Type': 'application/json'
-    ,'Access-Control-Allow-Origin': '*' 
   }
 });
 
